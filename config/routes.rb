fe80a90bedd09f44f96685e18fc55c8b43abc6ed
naonao-program <gf19a012@oecu.jp>
2022-06-23 18:43:55 +0900

    顧客側のルーティングを記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,26 @@
 Rails.application.routes.draw do
+  # 顧客側
   scope module: :public do
     root to: 'homes#top'
     get '/about' => 'homes#about'
     resources :items, only:[:index, :show]
+    resources :customers, only:[:show, :edit, :update] do
+      collection do
+        get 'withdraw'
+        patch 'unsubscribe'
+      end
+    end
+    resources :cart_items, only:[:index,:update,:create,:destroy] do
+      collection do
+        delete 'destroy_all'
+      end
+    end
+    resources :orders, only:[:new, :create, :index, :show] do
+      collection do
+        post 'comfirm'
+        get 'complete'
+      end
+    end
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
   end
 end
