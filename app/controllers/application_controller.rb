70b8beb195d85432fb10522311c4518c3f0fa8f7
naonao-program <gf19a012@oecu.jp>
2022-06-24 17:15:09 +0900

    顧客側の新規登録 ログインができるか確認しログイン後のマイページの遷移ができるように設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def after_sign_in_path_for(resource_or_scope)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin_admin_user
+      new_admin_admin_user_session_path
+    else
+      new_user_session_path
+    end
+  end
+
+  private
   def configure_permitted_parameters
    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, 
                                                       :last_name_kana, :first_name_kana, 
