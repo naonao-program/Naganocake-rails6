f8f3fdfc64702f89ccf64369c0fe57402e4765df
naonao-program <gf19a012@oecu.jp>
2022-06-29 16:18:30 +0900

    顧客側のeditページを作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,8 +1,25 @@
 class Public::CustomersController < ApplicationController
+  before_action :authenticate_user!, only:[:edit, :update]
   def show
     @customer = Customer.find(params[:id])
   end
   
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to customer_path(@customer.id)
+    else
+      render :edit
+    end
+  end
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana,
+                                     :post_code, :address, :phone_number, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
-  devise_for :customers,skip: [:passwords,], controllers: {
-    registrations: "customers/registrations",
-    sessions: 'customers/sessions'
-  }
+  devise_for :admins
+  devise_for :customers
   # 顧客側
   scope module: :public do
     root to: 'homes#top'
