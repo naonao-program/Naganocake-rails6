d585c04784faf98890f82686e037426815dafb96
naonao-program <gf19a012@oecu.jp>
2022-06-30 19:54:35 +0900

    色々変更

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,16 @@
+class Public::SessionsController < ApplicationController
+  before_action :customer_state, only: [:create]
+
+  protected
+# 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      redirect_to new_customer_registration
+    end
+  end
+end
