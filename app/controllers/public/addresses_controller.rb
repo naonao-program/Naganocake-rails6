091baf08b913eb0c833416cc94d4daf0521e897d
naonao-program <gf19a012@oecu.jp>
2022-08-02 00:38:03 +0900

    編集機能の実装

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -20,7 +20,13 @@ class Public::AddressesController < ApplicationController
   end
 
   def update
-    
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    if @address.save
+      redirect_to addresses_path
+    else
+      render :edit
+    end
   end
 
   private
