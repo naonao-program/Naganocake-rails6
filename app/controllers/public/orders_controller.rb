ac4da564a78d3c89914f9ab705760346c7e3cb72
naonao-program <gf19a012@oecu.jp>
2022-08-24 14:25:32 +0900

    新しい住所を送れるようにした

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,16 +3,27 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     address = Address.all
+    @addresses = current_customer.addresses
   end
 
   def comfirm
     @order = Order.new(order_params)
     @cart_items = CartItem.all
     @order.shipping_cost = 800
-    if params[:order][:select_address] == "0"
+    binding.pry
+    if params[:order][:address_option] == "0"
       @order.postal_code = current_customer.post_code
       @order.address = current_customer.address
       @order.name = current_customer.last_name + current_customer.first_name
+    elsif params[:order][:address_option] == "1"
+      shipment_address = Address.find(params[:order][:address])
+      @order.postal_code = shipment_address.postal_code
+      @order.address = shipment_address.address
+      @order.name = shipment_address.name
+    elsif params[:order][:address_option] == "2"
+      @order.postal_code = params[:order][:postal_code]
+      @order.address = params[:order][:address]
+      @order.name = params[:order][:name]
     end
   end
 
