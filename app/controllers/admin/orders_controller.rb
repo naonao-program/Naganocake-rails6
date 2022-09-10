2deba79a336c413bbf9e3331f0138b50280c7278
naonao-program <gf19a012@oecu.jp>
2022-09-10 11:57:22 +0900

    注文詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,34 @@
 class Admin::OrdersController < ApplicationController
   def index
-    @order = Order.all
+    @orders = Order.all
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @order_posts = @order.order_posts
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    if @order.update(update_status_params)
+      if @order.status == "0"
+        @order.order_details.each do |order_detail|
+          order_detail.making_status = "1"
+          order_detail.save!
+        end
+      end
+      flash[:notice] = '更新されました。'
+      redirect_to admin_orders_path
+    else
+      @order_details = @order.status
+      render :show
+    end
+  end
+
+  private
+  
+  def update_status_params
+    params.require(:order).permit(:status)
+  end
+
 end
